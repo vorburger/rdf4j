a4fe7826befd83909e8bd2ad4996eb9307b9eefe
Håvard Ottestad <hmottestad@gmail.com>
2023-12-26 07:51:03 +0100

    wip

---
+++ core/rio/jsonld-new/src/main/java/org/eclipse/rdf4j/rio/jsonld/JSONLDParser.java
@@ -0,0 +1,340 @@
+/*******************************************************************************
+ * Copyright (c) 2015 Eclipse RDF4J contributors, Aduna, and others.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Distribution License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *******************************************************************************/
+package org.eclipse.rdf4j.rio.jsonld;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.Reader;
+import java.util.Collection;
+import java.util.Set;
+import java.util.UUID;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.function.BiConsumer;
+
+import org.eclipse.rdf4j.model.BNode;
+import org.eclipse.rdf4j.model.IRI;
+import org.eclipse.rdf4j.model.Literal;
+import org.eclipse.rdf4j.model.Resource;
+import org.eclipse.rdf4j.model.Statement;
+import org.eclipse.rdf4j.model.Value;
+import org.eclipse.rdf4j.model.ValueFactory;
+import org.eclipse.rdf4j.rio.RDFFormat;
+import org.eclipse.rdf4j.rio.RDFHandler;
+import org.eclipse.rdf4j.rio.RDFHandlerException;
+import org.eclipse.rdf4j.rio.RDFParseException;
+import org.eclipse.rdf4j.rio.RDFParser;
+import org.eclipse.rdf4j.rio.RioSetting;
+import org.eclipse.rdf4j.rio.helpers.AbstractRDFParser;
+import org.eclipse.rdf4j.rio.helpers.JSONLDSettings;
+import org.eclipse.rdf4j.rio.helpers.JSONSettings;
+
+import jakarta.json.JsonObject;
+import jakarta.json.JsonString;
+import jakarta.json.JsonStructure;
+import jakarta.json.JsonValue;
+import no.hasmac.jsonld.JsonLd;
+import no.hasmac.jsonld.JsonLdOptions;
+import no.hasmac.jsonld.document.Document;
+import no.hasmac.jsonld.document.JsonDocument;
+import no.hasmac.jsonld.lang.Keywords;
+import no.hasmac.rdf.RdfConsumer;
+import no.hasmac.rdf.RdfValueFactory;
+
+/**
+ * An {@link RDFParser} that links to {@link JSONLDInternalTripleCallback}.
+ *
+ * @author Peter Ansell
+ */
+public class JSONLDParser extends AbstractRDFParser {
+
+	/**
+	 * Default constructor
+	 */
+	public JSONLDParser() {
+		super();
+	}
+
+	/**
+	 * Creates a JSONLD Parser using the given {@link ValueFactory} to create new {@link Value}s.
+	 *
+	 * @param valueFactory The ValueFactory to use
+	 */
+	public JSONLDParser(final ValueFactory valueFactory) {
+		super(valueFactory);
+	}
+
+	@Override
+	public RDFFormat getRDFFormat() {
+		return RDFFormat.JSONLD;
+	}
+
+	@Override
+	public Collection<RioSetting<?>> getSupportedSettings() {
+		Collection<RioSetting<?>> result = super.getSupportedSettings();
+
+		result.add(JSONSettings.ALLOW_BACKSLASH_ESCAPING_ANY_CHARACTER);
+		result.add(JSONSettings.ALLOW_COMMENTS);
+		result.add(JSONSettings.ALLOW_NON_NUMERIC_NUMBERS);
+		result.add(JSONSettings.ALLOW_NUMERIC_LEADING_ZEROS);
+		result.add(JSONSettings.ALLOW_SINGLE_QUOTES);
+		result.add(JSONSettings.ALLOW_UNQUOTED_CONTROL_CHARS);
+		result.add(JSONSettings.ALLOW_UNQUOTED_FIELD_NAMES);
+		result.add(JSONSettings.ALLOW_YAML_COMMENTS);
+		result.add(JSONSettings.ALLOW_TRAILING_COMMA);
+		result.add(JSONSettings.INCLUDE_SOURCE_IN_LOCATION);
+		result.add(JSONSettings.STRICT_DUPLICATE_DETECTION);
+
+		result.add(JSONLDSettings.DOCUMENT_LOADER);
+
+		return result;
+	}
+
+	@Override
+	public void parse(final InputStream in, final String baseURI)
+			throws IOException, RDFParseException, RDFHandlerException {
+		parse(in, null, baseURI);
+	}
+
+	@Override
+	public void parse(final Reader reader, final String baseURI)
+			throws IOException, RDFParseException, RDFHandlerException {
+		parse(null, reader, baseURI);
+	}
+
+	/**
+	 * Parse
+	 *
+	 * @param in
+	 * @param reader
+	 * @param baseURI
+	 * @throws IOException
+	 * @throws RDFParseException
+	 * @throws RDFHandlerException
+	 */
+	private void parse(InputStream in, Reader reader, String baseURI)
+			throws IOException, RDFParseException, RDFHandlerException {
+		clear();
+
+		try {
+
+			Document document;
+			if (in == null && reader != null) {
+				document = JsonDocument.of(reader);
+			} else if (in != null && reader == null) {
+				document = JsonDocument.of(in);
+			} else {
+				throw new IllegalArgumentException("Either in or reader must be set");
+			}
+
+			JsonLdOptions opts = new JsonLdOptions();
+
+			opts.setUriValidation(false);
+
+			RDFHandler rdfHandler = getRDFHandler();
+
+			JsonLd.toRdf(document).options(opts).base(baseURI).get(new RdfConsumer<>() {
+				@Override
+				public void handleTriple(Statement statement) {
+					rdfHandler.handleStatement(statement);
+				}
+
+				@Override
+				public void handleQuad(Statement statement) {
+					rdfHandler.handleStatement(statement);
+				}
+
+				@Override
+				public void handleNamespace(String prefix, String uri) {
+
+				}
+			}, new RdfValueFactory<Statement, Statement, IRI, BNode, Resource, Literal, Value>() {
+				@Override
+				public Statement createTriple(Resource subject, IRI predicate, Value object) {
+					return valueFactory.createStatement(subject, predicate, object);
+				}
+
+				@Override
+				public Statement createQuad(Resource subject, IRI predicate, Value object, Resource graphName) {
+					return valueFactory.createStatement(subject, predicate, object, graphName);
+				}
+
+				@Override
+				public Statement createQuad(Statement statement, Resource graphName) {
+					return valueFactory.createStatement(statement.getSubject(), statement.getPredicate(),
+							statement.getObject(), graphName);
+				}
+
+				@Override
+				public IRI createIRI(String value) {
+					return valueFactory.createIRI(value);
+				}
+
+				@Override
+				public BNode createBlankNode(String value) {
+					return (BNode) createNode(value);
+				}
+
+				@Override
+				public Literal createTypedLiteral(String value, String datatype) {
+					return valueFactory.createLiteral(value, valueFactory.createIRI(datatype));
+				}
+
+				@Override
+				public Literal createString(String value) {
+					return valueFactory.createLiteral(value);
+				}
+
+				@Override
+				public Literal createLangString(String value, String lang) {
+					return valueFactory.createLiteral(value, lang);
+				}
+			});
+
+			extractPrefixes(document, rdfHandler::handleNamespace);
+
+		} catch (no.hasmac.jsonld.JsonLdError e) {
+			throw new RDFParseException("Could not parse JSONLD", e);
+		} catch (RuntimeException e) {
+			if (e.getCause() != null && e.getCause() instanceof RDFParseException) {
+				throw (RDFParseException) e.getCause();
+			}
+			throw e;
+		} finally {
+			clear();
+		}
+	}
+
+	private static void extractPrefixes(Document document, BiConsumer<String, String> action) {
+		try {
+			JsonStructure js = document.getJsonContent().get();
+			switch (js.getValueType()) {
+			case ARRAY:
+				extractPrefixes(js, action);
+				break;
+			case OBJECT:
+				JsonValue jv = js.asJsonObject().get(Keywords.CONTEXT);
+				extractPrefixes(jv, action);
+				break;
+			default:
+				break;
+			}
+		} catch (Throwable ex) {
+			// TODO
+			throw new RuntimeException(ex);
+		}
+	}
+
+	private static void extractPrefixes(JsonValue jsonValue, BiConsumer<String, String> action) {
+		if (jsonValue == null) {
+			return;
+		}
+		// JSON-LD 1.1 section 9.4
+		switch (jsonValue.getValueType()) {
+		case ARRAY:
+			jsonValue.asJsonArray().forEach(jv -> extractPrefixes(jv, action));
+			break;
+		case OBJECT:
+			extractPrefixesCxtDefn(jsonValue.asJsonObject(), action);
+			break;
+		case NULL:
+			break; // We are only interested in prefixes
+		case STRING:
+			break; // We are only interested in prefixes
+		default:
+			break;
+		}
+	}
+
+	private static void extractPrefixesCxtDefn(JsonObject jCxt, BiConsumer<String, String> action) {
+		Set<String> keys = jCxt.keySet();
+		keys.forEach(k -> {
+			// "@vocab" : "uri"
+			// "shortName" : "uri"
+			// "shortName" : { "@type":"@id" , "@id": "uri" } -- presumed to be a single property aliases, not a prefix.
+			JsonValue jvx = jCxt.get(k);
+			if (JsonValue.ValueType.STRING != jvx.getValueType()) {
+				return;
+			}
+			String prefix = k;
+			if (Keywords.VOCAB.equals(k)) {
+				prefix = "";
+			} else if (k.startsWith("@"))
+			// Keyword, not @vocab.
+			{
+				return;
+			}
+			// Pragmatic filter: URI ends in "#" or "/" or ":"
+			String uri = JsonString.class.cast(jvx).getString();
+			if (uri.endsWith("#") || uri.endsWith("/") || uri.endsWith(":")) {
+				action.accept(prefix, uri);
+				return;
+			}
+		});
+	}
+
+//
+//
+//	/**
+//	 * Get an instance of JsonFactory configured using the settings from {@link #getParserConfig()}.
+//	 *
+//	 * @return A newly configured JsonFactory based on the currently enabled settings
+//	 */
+//	private JsonFactory configureNewJsonFactory() {
+//		JsonFactoryBuilder builder = new JsonFactoryBuilder();
+//		ParserConfig parserConfig = getParserConfig();
+//
+//		if (parserConfig.isSet(JSONSettings.ALLOW_BACKSLASH_ESCAPING_ANY_CHARACTER)) {
+//			builder.configure(JsonReadFeature.ALLOW_BACKSLASH_ESCAPING_ANY_CHARACTER,
+//					parserConfig.get(JSONSettings.ALLOW_BACKSLASH_ESCAPING_ANY_CHARACTER));
+//		}
+//		if (parserConfig.isSet(JSONSettings.ALLOW_COMMENTS)) {
+//			builder.configure(JsonReadFeature.ALLOW_JAVA_COMMENTS,
+//					parserConfig.get(JSONSettings.ALLOW_COMMENTS));
+//		}
+//		if (parserConfig.isSet(JSONSettings.ALLOW_NON_NUMERIC_NUMBERS)) {
+//			builder.configure(JsonReadFeature.ALLOW_NON_NUMERIC_NUMBERS,
+//					parserConfig.get(JSONSettings.ALLOW_NON_NUMERIC_NUMBERS));
+//		}
+//		if (parserConfig.isSet(JSONSettings.ALLOW_NUMERIC_LEADING_ZEROS)) {
+//			builder.configure(JsonReadFeature.ALLOW_LEADING_ZEROS_FOR_NUMBERS,
+//					parserConfig.get(JSONSettings.ALLOW_NUMERIC_LEADING_ZEROS));
+//		}
+//		if (parserConfig.isSet(JSONSettings.ALLOW_SINGLE_QUOTES)) {
+//			builder.configure(JsonReadFeature.ALLOW_SINGLE_QUOTES,
+//					parserConfig.get(JSONSettings.ALLOW_SINGLE_QUOTES));
+//		}
+//		if (parserConfig.isSet(JSONSettings.ALLOW_UNQUOTED_CONTROL_CHARS)) {
+//			builder.configure(JsonReadFeature.ALLOW_UNESCAPED_CONTROL_CHARS,
+//					parserConfig.get(JSONSettings.ALLOW_UNQUOTED_CONTROL_CHARS));
+//		}
+//		if (parserConfig.isSet(JSONSettings.ALLOW_UNQUOTED_FIELD_NAMES)) {
+//			builder.configure(JsonReadFeature.ALLOW_UNQUOTED_FIELD_NAMES,
+//					parserConfig.get(JSONSettings.ALLOW_UNQUOTED_FIELD_NAMES));
+//		}
+//		if (parserConfig.isSet(JSONSettings.ALLOW_YAML_COMMENTS)) {
+//			builder.configure(JsonReadFeature.ALLOW_YAML_COMMENTS,
+//					parserConfig.get(JSONSettings.ALLOW_YAML_COMMENTS));
+//		}
+//		if (parserConfig.isSet(JSONSettings.ALLOW_TRAILING_COMMA)) {
+//			builder.configure(JsonReadFeature.ALLOW_TRAILING_COMMA,
+//					parserConfig.get(JSONSettings.ALLOW_TRAILING_COMMA));
+//		}
+//		if (parserConfig.isSet(JSONSettings.INCLUDE_SOURCE_IN_LOCATION)) {
+//			builder.configure(StreamReadFeature.INCLUDE_SOURCE_IN_LOCATION,
+//					parserConfig.get(JSONSettings.INCLUDE_SOURCE_IN_LOCATION));
+//		}
+//		if (parserConfig.isSet(JSONSettings.STRICT_DUPLICATE_DETECTION)) {
+//			builder.configure(StreamReadFeature.STRICT_DUPLICATE_DETECTION,
+//					parserConfig.get(JSONSettings.STRICT_DUPLICATE_DETECTION));
+//		}
+//		return builder.build().setCodec(JSON_MAPPER);
+//	}
+}
